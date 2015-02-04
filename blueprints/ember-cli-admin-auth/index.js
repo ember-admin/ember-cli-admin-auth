ed89d33046e8a01c42d2836561abdeaf4bc0695b
Igor Kur <kur91@mail.ru>
2015-02-04 11:51:00 +0200

    fix blueprints

--- blueprints/ember-cli-admin-auth/index.js
+++ blueprints/ember-cli-admin-auth/index.js
@@ -1,4 +1,4 @@
-var processTextContent = require('node_modules/ember-cli-admin/lib/proccess-text-content').processTextContent;
+var processTextContent = require('ember-cli-admin-auth/lib/proccess-text-content').processTextContent;
 
 module.exports = {
   description: 'Generates ember-simple-auth components and settings',
@@ -24,13 +24,21 @@ module.exports = {
     process('app/router.js',
       {
         replace: {
-          "Router.map(function() {": "Router.map(function() {\n\treturn this.route(\"login\"});\n"
+          "Router.map(function() {": "Router.map(function() {\n\tthis.route(\"login\");\n"
         }
       });
     process('config/environment.js',
       {
         insert: {
-          "\t};": "\n\tENV['simple-auth'] = {\n\t\tauthorizer: 'simple-auth-authorizer:devise',\n\t\trouteAfterAuthentication: '/'\n\t};\n"
+          "if (environment === 'development') {": {content: "ENV['simple-auth'] = {\n\t\tauthorizer: 'simple-auth-authorizer:devise',\n\t\trouteAfterAuthentication: '/'\n\t};\n\n\t", before: true},
+          "if (environment === 'development') {": {content: "\n\t\tENV['simple-auth-devise'] = {\n\t\t\tresourceName: 'user',\n\t\t\tserverTokenEndpoint: '/api/v1/users/sign_in'\n\t\t};\n\t\tENV['simple-auth'].crossOriginWhitelist = ['*'];\n", after: true}
+        }
+      });
+    process('app/routes/main.js',
+      {
+        insert: {
+          "'ember-cli-admin/mixins/routes/base';": {content: "\nimport ApplicationRouteMixin from 'simple-auth/mixins/application-route-mixin';\n\nBaseAdminRouteMixin.reopen(ApplicationRouteMixin);\n", after: true},
+          "extend(BaseAdminRouteMixin": {content: ", {\n\tredirect: function(){\n\t\tif(!this.get('session.isAuthenticated')){\n\t\t\tthis.transitionTo('login');\n\t\t}\n\t}\n}" , after: true}
         }
       });
   }
